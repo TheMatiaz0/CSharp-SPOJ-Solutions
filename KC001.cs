4a764ca8fde096a53e1d0b0ec638fed4d15e37b6
TheMatiaz0 <37487149+TheMatiaz0@users.noreply.github.com>
2020-01-19 01:16:07 +0100

    KC001

---
+++ KC001.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class KC001
+{
+	static void Main ()
+	{
+		int num1 = int.Parse(Console.ReadLine());
+		int num2 = int.Parse(Console.ReadLine());
+		int num3 = int.Parse(Console.ReadLine());
+
+		Add(num1, num2, num3);
+	}
+
+	private static void Add (int num1, int num2, int num3)
+	{
+		Console.WriteLine(num1 + num2 + num3);
+	}
+}
