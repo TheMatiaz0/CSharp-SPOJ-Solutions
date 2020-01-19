fe5e51c235feaed0d0afb04e6e7795b008c309d0
TheMatiaz0 <37487149+TheMatiaz0@users.noreply.github.com>
2020-01-19 14:35:53 +0100

    POTSAM

--- KC001.cs
+++ KC001.cs
@@ -4,19 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// KC001 - Dodawanie liczb całkowitych
+// https://pl.spoj.com/problems/KC001
+
 public class KC001
 {
-	static void Main ()
+	static void Main()
 	{
 		int num1 = int.Parse(Console.ReadLine());
 		int num2 = int.Parse(Console.ReadLine());
 		int num3 = int.Parse(Console.ReadLine());
 
-		Add(num1, num2, num3);
+		Console.Write(Add(num1, num2, num3));
 	}
 
-	private static void Add (int num1, int num2, int num3)
+	/// <summary>
+	/// Add three integers and return sum.
+	/// </summary>
+	/// <param name="num1"></param>
+	/// <param name="num2"></param>
+	/// <param name="num3"></param>
+	/// <returns></returns>
+	private static int Add(int num1, int num2, int num3)
 	{
-		Console.WriteLine(num1 + num2 + num3);
+		return num1 + num2 + num3;
 	}
 }
---
+++ POTSAM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// POTSAM - Samolot
+// https://pl.spoj.com/problems/POTSAM
+
+public class POTSAM
+{
+	static void Main()
+	{
+		string input = Console.ReadLine();
+		string[] words = input.Split(null);
+
+		int n1 = int.Parse(words[0]);
+		int k1 = int.Parse(words[1]);
+		int n2 = int.Parse(words[2]);
+		int k2 = int.Parse(words[3]);
+
+		Console.Write((n1 * k1) + (n2 * k2));
+	}
+}
