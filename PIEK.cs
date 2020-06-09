81bdaeecce16feb1cf4410332d44377aedee2b44
TheMatiaz0 <37487149+TheMatiaz0@users.noreply.github.com>
2020-06-10 00:55:02 +0200

    PIEK in progress

---
+++ PIEK.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class PIEK
+{
+	static void Main ()
+	{
+		int bakeryCount = int.Parse(Console.ReadLine());
+		int[,] distanceBetweenBakeries = new int[,] { { } }
+
+		// int[,] arr = new int[bakeryCount, bakeryCount];
+		Console.ReadLine();
+	}
+}
